4023516eb3b6b64682bc6f11d5453b8062be8e15
kikesan <kikin1601@hotmail.com>
2018-12-12 23:35:06 -0500

    Preparando el ambiente de trabajo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react'
+import Header from './containers/Header'
+import Sidebar from './containers/Sidebar'
+import Body from './containers/Body'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTasks, faChevronLeft, faChevronRight, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import '../node_modules/bootstrap/scss/bootstrap.scss'
 import './styles/index.scss'
-import Counter from './containers/Counter'
 
-export default class App extends Component {
-  state = {
-    message: 'Message'
-  }
+library.add(faTasks, faChevronLeft, faChevronRight, fab, faHeart)
 
-  handleToggleMessage = (message) => {
-    this.setState({ message })
-  }
+class App extends Component {
 
   render() {
     return (
-      <div>
-        <div>
-          <Counter />
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <Sidebar />
+          <Body />
         </div>
+        <footer className="container-fluid credits text-center">
+          <FontAwesomeIcon icon="chevron-left" />
+          <FontAwesomeIcon icon="chevron-right" />
+          <span className="credit-text">with</span>
+          <FontAwesomeIcon icon="heart" />
+          <span className="credit-text">and</span>
+          <FontAwesomeIcon icon={['fab', 'react']} size="lg" />
+          <span className="credit-text">by Kike</span>
+        </footer>
       </div>
     )
   }
 }
+
+export default App
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux'
 import counter from './counter'
 import credit from './credits'
+import categorias from './categorias'
 
 const reducers = combineReducers({
-  counter,
-  credit
+  credit,
+  categorias
 })
 
 export default reducers
